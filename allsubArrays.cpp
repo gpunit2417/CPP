436f8f58d5b51719f100d54487ee2ae5eb57b31f
Punit goyal <118668663+gpunit2417@users.noreply.github.com>
2024-09-13 18:39:34 +0530

    array code

---
+++ allsubArrays.cpp
@@ -0,0 +1,70 @@
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cout<<"enter size"<<endl;
+//     cin>>n;
+
+//     int arr[n];
+//     cout<<"array elements"<<endl;
+//     for(int i=0; i<n; i++){
+//         cin>>arr[i];
+//     }
+
+//     for(int i=0; i<n; i++){
+//         for(int j=i; j<n; j++){
+//             for(int k=i; k<=j; k++){
+//                 cout<<arr[k]<<" ";
+//             }
+//             cout<<endl;
+//         }
+//     }
+//     return 0;
+// }
+
+
+
+//
+// #include<iostream>
+// using namespace std;
+// class Student{
+//     public:
+//     int* age;
+//     Student(int a){
+//         age = new int;
+//         *age = a;
+//     }
+//     Student(const Student &s){
+//         age = s.age;
+//     }
+
+//     void print(){
+//         cout<<"print"<<endl;
+//     }
+// };
+
+// int main(){
+//     if(!(cout<<"i am")){
+//         cout<<"de";
+//     }
+//     else{
+//         cout<<" v";
+//     }
+
+//     return 0;
+// }
+
+// int main(){
+//     int *p;
+//     void *vp;
+//     if(vp == p){
+//         cout<<"e";
+//     }
+//     return 0;
+// }
+
+// int main(){
+//     int a=4;
+//     cout<<~a;
+// }
