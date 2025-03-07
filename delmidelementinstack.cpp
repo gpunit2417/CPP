10ce7068bed515152485b49bffd531bba1731e67
Punit goyal <118668663+gpunit2417@users.noreply.github.com>
2025-03-07 21:44:48 +0530

    Update delmidelementinstack.cpp

--- delmidelementinstack.cpp
+++ delmidelementinstack.cpp
@@ -7,14 +7,14 @@ void solve(stack<int>&inputStack, int count, int size){
     }
 
     int num = inputStack.top();
-    inputStack.top();
+    inputStack.pop();
 
     //recursion
     solve(inputStack, count+1, size);
     inputStack.push(num);
 }
 
-void deleteMiddle(stack<int>&nputStack, int N){
+void deleteMiddle(stack<int>inputStack, int N){
     int count = 0;
     solve(inputStack, count, N);
 }
