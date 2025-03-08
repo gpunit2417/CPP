152fdf4739b46ba8d777f104007c9dd0dd75d64b
Punit goyal <118668663+gpunit2417@users.noreply.github.com>
2025-03-08 19:03:36 +0530

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -1,27 +1,44 @@
 #include <iostream>
-#include<unordered_map>
-#include<list>
+#include <unordered_map>
+#include <list>
 using namespace std;
 
-class graph {
-    public:
-    unordered_map<int, list<int> adj;
+class Graph {
+public:
+    unordered_map<int, list<int>> adj;  // Corrected syntax
 
-    void addEdge(int  u, list v,bool direction){
-        // direction -0->unidirected
-        // direction -1->directed graph
+    void addEdge(int u, int v, bool direction) {
+        // direction == 0 -> Undirected graph
+        // direction == 1 -> Directed graph
 
-        // craeted an edge from u to va_list
-        adj[u].push_back[v];
-        if (direction==0)
-        {
-            di
+        // Create an edge from u to v
+        adj[u].push_back(v);
+
+        // If undirected, add edge from v to u
+        if (direction == 0) {
+            adj[v].push_back(u);
         }
+    }
 
+    void printGraph() {
+        for (auto node : adj) {
+            cout << node.first << " -> ";
+            for (int neighbor : node.second) {
+                cout << neighbor << " ";
+            }
+            cout << endl;
+        }
     }
 };
 
-int main(){
-    return 0;
+int main() {
+    Graph g;
+    g.addEdge(1, 2, 0); // Undirected edge between 1 and 2
+    g.addEdge(2, 3, 1); // Directed edge from 2 to 3
+    g.addEdge(3, 4, 0); // Undirected edge between 3 and 4
 
+    cout << "Graph adjacency list:\n";
+    g.printGraph();
+
+    return 0;
 }
