d071faf71448c4194d68209a2579ec9289e46d43
Punit goyal <118668663+gpunit2417@users.noreply.github.com>
2025-03-09 11:24:53 +0530

    Update in-posttree.cpp

--- in-posttree.cpp
+++ in-posttree.cpp
@@ -27,7 +27,7 @@ class Solution{
         int postindex = 0; 
         map<int,int> nodetoindex;
         mapp(in, nodetoindex, n);
-        Node* ans = solve(in, post, postindex, 0, n-1, n);
+        Node* ans = solve(in, post, postindex, 0, n-1, n, nodetoindex);
         return ans;
     }
 };
