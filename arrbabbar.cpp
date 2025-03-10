03c05cbc4cd39cc804b3af679405af888c9e9196
Punit goyal <118668663+gpunit2417@users.noreply.github.com>
2025-03-10 21:30:23 +0530

    Update arrbabbar.cpp

--- arrbabbar.cpp
+++ arrbabbar.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int number[15];
+    int arr[5];
+    for(int i=0; i<5; i++){
+        cin>>arr[i];
+    }
+    cout<<"Elements are:"<<endl;
+    for(int i=0; i<5; i++){
+        cout<<arr[i]<<endl;
+    }
     return 0;
 }
