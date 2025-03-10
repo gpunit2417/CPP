dbcff683bfa28b663f1a7f8c4b90b81b2be6153c
Punit goyal <118668663+gpunit2417@users.noreply.github.com>
2025-03-10 21:31:21 +0530

    added comments

--- arrbabbar.cpp
+++ arrbabbar.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int main(){
     int arr[5];
-    for(int i=0; i<5; i++){
+    for(int i=0; i<5; i++){        //input from the user
         cin>>arr[i];
     }
     cout<<"Elements are:"<<endl;
-    for(int i=0; i<5; i++){
+    for(int i=0; i<5; i++){        //printing the array elements
         cout<<arr[i]<<endl;
     }
     return 0;
